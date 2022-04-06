a4af96d85b72e93e254a36e21d6531a015f32a1c
Jorge Rodrigo <jorge.rodrigo.481516@gmail.com>
2022-04-06 16:47:48 -0300

    Add funcionalidades dos botões

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -1,16 +1,26 @@
-// el(s): elemento(s)
+// el(s) = elemento(s)
 
 const elsNumberBtn = document.getElementsByClassName("number-btn"),
+  elsOperationBtn = document.getElementsByClassName("operation-btn"),
   elViewInput = document.getElementById("view-input"),
+  elEqualBtn = document.getElementById("equal-btn"),
   elClearBtn = document.getElementById("clear-btn"),
-  elPeriodBtn = document.getElementById("period-btn");
+  elPeriodBtn = document.getElementById("period-btn"),
+  elValueOne = document.getElementById("value-one"),
+  elOperation = document.getElementById("operation"),
+  elValueTwo = document.getElementById("value-two"),
+  elErrorMsg = document.getElementById("error-msg"),
+  elResult = document.getElementById("result"),
+  elRefreshBtn = document.getElementById("refresh-btn");
 
 const numbersText = "0123456789";
 
 let hasPeriod = false,
   afterPeriod = false,
+  isValid,
   indexOfPeriod,
-  newViewInput;
+  newViewInput,
+  result;
 
 function addEventListenerToClass(elsClass, event, func) {
   let elsClassLength = elsClass.length;
@@ -19,14 +29,31 @@ function addEventListenerToClass(elsClass, event, func) {
   }
 }
 
+function setStyleToClass(elsClass, styleAtributte, atributteValue) {
+  let elsClassLength = elsClass.length;
+  for (let i = 0; i < elsClassLength; i++) {
+    elsClass[i].style[styleAtributte] = atributteValue;
+  }
+}
+
+function elErrorMsgAppear() {
+  elErrorMsg.style.filter = "opacity(100%)";
+  elErrorMsg.style.marginTop = "10px";
+}
+
+function elErrorMsgDisappear() {
+  elErrorMsg.style.filter = "opacity(0%)";
+  elErrorMsg.style.marginTop = "-10px";
+}
+
 function putOnViewInput() {
   if (hasPeriod && !afterPeriod) {
     indexOfPeriod = elViewInput.value.indexOf(".");
     newViewInput = elViewInput.value.slice(0, indexOfPeriod);
-    elViewInput.value = `${newViewInput}.${this.innerHTML}`;
+    elViewInput.value = `${newViewInput}.${this.textContent}`;
     afterPeriod = true;
   } else {
-    elViewInput.value += this.innerHTML;
+    elViewInput.value += this.textContent;
   }
 }
 
@@ -49,8 +76,8 @@ function pressCharacter(char) {
     hasPeriod = true;
   }
   if (hasPeriod && !afterPeriod) {
-    // espera que seja posto o character no elViewInput
-    // e só então o altera
+    // espera até que seja posto o character
+    // no elViewInput e só então o altera
     setTimeout(() => {
       indexOfPeriod = elViewInput.value.indexOf(".");
       newViewInput = elViewInput.value.slice(0, indexOfPeriod);
@@ -63,7 +90,89 @@ function pressCharacter(char) {
   char.key == "Backspace" ? clearViewInput() : console.log(char.key);
 }
 
+function validate() {
+  if (elViewInput.value == "") {
+    elErrorMsg.textContent = "ERROR: Primeiramente adicione algum valor.";
+    elErrorMsgAppear();
+    isValid = false;
+  } else {
+    isValid = true;
+  }
+}
+
+function setOperation() {
+  validate();
+  if (isValid) {
+    if (elValueOne.textContent == "") {
+      elErrorMsgDisappear();
+      elValueOne.textContent = elViewInput.value;
+      elOperation.textContent = this.textContent;
+      setStyleToClass(elsOperationBtn, "filter", "grayscale(100%)");
+      setStyleToClass(elsOperationBtn, "cursor", "not-allowed");
+      clearViewInput();
+    } else {
+      elErrorMsg.textContent = "ERROR: Operação matemática já foi aplicada.";
+      elErrorMsgAppear();
+    }
+  }
+}
+
+function putResult() {
+  validate();
+  if (isValid) {
+    if (elValueOne.textContent != "" && elValueTwo.textContent == "") {
+      elErrorMsgDisappear();
+      elValueTwo.textContent = elViewInput.value;
+
+      switch (elOperation.textContent) {
+        case "+":
+          result =
+            parseFloat(elValueOne.textContent) +
+            parseFloat(elValueTwo.textContent);
+          break;
+
+        case "-":
+          result =
+            parseFloat(elValueOne.textContent) -
+            parseFloat(elValueTwo.textContent);
+          break;
+
+        case "x":
+          result =
+            parseFloat(elValueOne.textContent) *
+            parseFloat(elValueTwo.textContent);
+          break;
+
+        case "÷":
+          result =
+            parseFloat(elValueOne.textContent) /
+            parseFloat(elValueTwo.textContent);
+          break;
+
+        default:
+          break;
+      }
+      elResult.textContent = result.toFixed(2);
+      elEqualBtn.style.filter = "grayscale(100%)";
+      elEqualBtn.style.cursor = "not-allowed";
+      clearViewInput();
+    } else if (elValueOne.textContent == "") {
+      elErrorMsg.textContent = "ERROR: Antes escolha uma operação matemática.";
+      elErrorMsgAppear();
+    } else {
+      elErrorMsg.textContent =
+        "ERROR: O cálculo já foi realizado, recarregue a página.";
+      elErrorMsgAppear();
+    }
+  }
+}
+
 addEventListenerToClass(elsNumberBtn, "click", putOnViewInput);
+addEventListenerToClass(elsOperationBtn, "click", setOperation);
+elEqualBtn.addEventListener("click", putResult);
 elClearBtn.addEventListener("click", clearViewInput);
 elPeriodBtn.addEventListener("click", addPeriod);
 elViewInput.addEventListener("keydown", pressCharacter);
+elRefreshBtn.addEventListener("click", () => {
+  document.location.reload(true);
+});
